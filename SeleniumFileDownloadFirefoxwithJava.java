14eb65997be0934d39b49928fcb95cc99e3da5f2
sarah-mahmood-dotcom <78915351+sarah-mahmood-dotcom@users.noreply.github.com>
2021-06-21 16:32:48 +0500

    Add files via upload

---
+++ SeleniumFileDownloadFirefoxwithJava.java
@@ -0,0 +1,18 @@
+
+
+
+// To download file in gecko
+String downloadFilepath = "Your Download Path";
+
+FirefoxOptions options = new FirefoxOptions();
+options.setAcceptInsecureCerts(true);
+options.addPreference("browser.download.folderList", 1);
+options.addPreference("browser.helperApps.alwaysAsk.force", false);
+options.addPreference("browser.download.dir", downloadFilepath); 
+options.addPreference("browser.download.defaultFolder",downloadFilepath); 
+options.addPreference("browser.download.manager.showWhenStarting", false);
+options.addPreference("browser.helperApps.neverAsk.saveToDisk","application/download, application/excel, application/octet-stream, text/csv"); //;application/octet-stream
+//options.addPreference("browser.helperApps.neverAsk.saveToDisk","multipart/x-zip,application/zip,application/x-zip-compressed,application/x-compressed,application/msword,application/csv,text/csv,image/png ,image/jpeg, application/pdf, text/html,text/plain,  application/excel, application/vnd.ms-excel, application/x-excel, application/x-msexcel, application/octet-stream");
+FirefoxDriver driver = new FirefoxDriver(options);
+
+driver.navigate().to("Your Site");
